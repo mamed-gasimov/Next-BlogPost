de7ddd9bdb2b91dff554cc0826a65dc244c5942d
Mammadali Gasimov <mamed-gasimov@Mamed.local>
2022-03-09 00:09:09 +0400

    changed first post

--- components/Posts/PostDetail/PostContent/PostContent.js
+++ components/Posts/PostDetail/PostContent/PostContent.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import { PostHeader } from '../PostHeader';
 import styles from './PostContent.module.css';
@@ -5,13 +6,38 @@ import styles from './PostContent.module.css';
 const PostContent = ({ post }) => {
     const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+    const customRenderers = {
+        p(paragraph) {
+            const { node } = paragraph;
+            let result = null;
+
+            if (node.children[0].tagName === 'img') {
+                const image = node.children[0];
+                result = (
+                    <div className={styles.image}>
+                        <Image
+                            src={`/images/posts/${post.slug}/${image.properties.src}`}
+                            alt={image.alt}
+                            width={600}
+                            height={300}
+                        />
+                    </div>
+
+                )
+            } else {
+                result = <p>{paragraph.children}</p>
+            }
+            return result;
+        }
+    }
+
     return (
-        <article className={styles.content}>
+        <article className={styles.content} >
             <PostHeader title={post.title} image={imagePath} />
-            <ReactMarkdown>
+            <ReactMarkdown components={customRenderers}>
                 {post.content}
             </ReactMarkdown>
-        </article>
+        </article >
     )
 }
 
